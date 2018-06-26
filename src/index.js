d4116fd3086d5cf5dc67cdd7db90e56ca8aaaad6
genshun <genshun9@gmail.com>
2018-06-27 01:24:19 +0900

    elm時代に作られたindex.jsを削除

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './client/main.css';
-import { Main } from './client/Main.elm';
-import registerServiceWorker from './client/registerServiceWorker';
-
-Main.embed(document.getElementById('root'));
-
-registerServiceWorker();

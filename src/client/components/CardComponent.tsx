7f7177adf00d9e05ad7f93c57f3bce645f18095c
genshun <genshun9@gmail.com>
2018-06-28 00:59:42 +0900

    Cardコンポーネントの完成

--- src/client/components/CardComponent.tsx
+++ src/client/components/CardComponent.tsx
@@ -2,14 +2,13 @@ import * as React from "react";
 import Card from "../models/Card";
 
 export interface CardProps {
-  key: number;
   card: Card;
   onClickCard: Function;
 }
 
 const CardComponent: React.SFC<CardProps> = props => {
   return (
-    <span key={props.key} style={{padding: 4}}>
+    <span style={{padding: 4}}>
       <img src={`../../../src/assets/${props.card.cardURL}.jpg`}
            style={{width: 160, height: 232}}
            onClick={() => props.onClickCard(props.card.cardID)}/>
--- src/client/components/DraftComponent.tsx
+++ src/client/components/DraftComponent.tsx
@@ -4,13 +4,17 @@ import CardComponent from "./CardComponent";
 import Card from "../models/Card";
 
 const DraftComponent: React.SFC<DraftProps> = props => {
-  const src = ['4HZgkF5eTAsS', '4Sq6hpEqdAsS', '8lTB9DmqJh1F', 'iVIsWINQHhCU'];
+  const src = ['4HZgkF5eTAsS', '4Sq6hpEqdAsS', '8lTB9DmqJh1F', 'iVIsWINQHhCU', 'T1Tr04WCwUZB', 'AHWIVNomv6qk'];
   const card:Card[] = src.map(s => new Card({name: s, cardID: s, cardURL: s}));
   return (
     <div>
-      {card.map((c,i) => (
-        <CardComponent key={i} card={c} onClickCard={cardID => props.onClickCard(cardID)}/>
-      ))}
+      {card.map((c, i) => (
+            <span key={`card-${i}`}>
+              {(i + 1) % 5 === 0 ? <p/> : null}
+            <CardComponent card={c} onClickCard={cardID => props.onClickCard(cardID)}/>
+            </span>
+        )
+      )}
     </div>
   )
 };

f2dc89743bf4ead1919202d0b0ca9c70e0bffcf3
genshun <genshun9@gmail.com>
2018-07-01 23:20:19 +0900

    ドラフト終了後にデッキレシピが表示されなかったバグを修正

--- src/client/components/DeckListComponent.tsx
+++ src/client/components/DeckListComponent.tsx
@@ -5,7 +5,7 @@ import {DeckListProps} from "../containers/DeckListContainer";
 const DeckListComponent: React.SFC<DeckListProps> = props => {
   return (
     <div>
-      {props.me.handCardList.map((c, i) => (
+      {props.me.draftDeckList.map((c, i) => (
           <span key={`card-${i}`}>
               {i % 10 === 0 ? <p/> : null}
             <SimpleCardComponent card={c} />

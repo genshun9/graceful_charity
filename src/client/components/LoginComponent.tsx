a2e9f57e2084dac15b34d60a406a16ae33c75e18
genshun <genshun9@gmail.com>
2018-07-08 19:24:35 +0900

    ログイン画面を修正

--- src/client/components/LoginComponent.tsx
+++ src/client/components/LoginComponent.tsx
@@ -1,18 +1,36 @@
 import * as React from 'react';
 import {LoginProps} from "../containers/LoginContainer";
+import {Button, ControlLabel, Form, FormControl, FormGroup} from "react-bootstrap";
+import {PLAYER_MAX_NUMBER} from "../constants/ClientApplicationConstants";
 
 const LoginComponent: React.SFC<LoginProps> = props => {
   return (
     <div>
       <br/>
-      <span style={{paddingLeft: 20}}>
-      <input disabled={props.player !== null} value={props.inputPlayerName}
-             onChange={e => props.onChangePlayerName(e.target.value)}/>
-      </span>
-      <span style={{paddingLeft: 20}}>
-        <button disabled={props.player !== null || props.inputPlayerName === ""}
-                onClick={() => props.onClickSendPlayerName(props.inputPlayerName, props.randomID)}>送信</button>
-      </span>
+      <Form inline>
+        <span style={{paddingLeft: 20}}>
+          <FormGroup controlId="formInlineName">
+            <ControlLabel>ユーザー名</ControlLabel>
+              <span style={{paddingLeft: 10}}>
+              </span>
+            <FormControl type="text"
+                         placeholder="名前を入力してね"
+                         disabled={props.player !== null}
+                         value={props.inputPlayerName}
+                         onChange={e => props.onChangePlayerName((e.target as any).value)}/>
+          </FormGroup>
+        </span>
+        <span style={{paddingLeft: 10}}>
+        <Button type="submit"
+                disabled={props.player !== null || props.inputPlayerName === ""}
+                onClick={() => props.onClickSendPlayerName(props.inputPlayerName, props.randomID)}>
+          ログインする
+        </Button>
+        </span>
+      </Form>
+      <br/>
+      {props.player !== null ? <div style={{paddingLeft: 20, fontSize: 20}}>{PLAYER_MAX_NUMBER}人集まるまでお待ち下さい</div> : null}
+      <br/>
       {props.players.map((p, i) => {
         const style = (props.player === null || p.playerID !== props.player.playerID) ? {
           color: "black",
--- src/client/constants/ClientApplicationConstants.ts
+++ src/client/constants/ClientApplicationConstants.ts
@@ -1,1 +1,2 @@
-export const PUBLIC_IP = 'localhost';
+export const PUBLIC_IP:string = 'localhost';
+export const PLAYER_MAX_NUMBER: number = 6;

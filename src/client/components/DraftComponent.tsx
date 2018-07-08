7984b6597172fc6e690b7817b286310cee0746b7
genshun <genshun9@gmail.com>
2018-07-08 21:09:15 +0900

    ドラフト画面を修正

--- src/client/components/DraftComponent.tsx
+++ src/client/components/DraftComponent.tsx
@@ -3,68 +3,94 @@ import {DraftProps} from "../containers/DraftContainer";
 import CardComponent from "./CardComponent";
 import {Button, OverlayTrigger, Popover} from 'react-bootstrap'
 import {GAME_PROGRESS} from "../../common/constants/Enums";
+import {ROTATION_MAX_NUMBER} from "../constants/ClientApplicationConstants";
 
 const DraftComponent: React.SFC<DraftProps> = props => {
   const pickButton = (
     <span style={{paddingLeft: 20, paddingRight: 20}}>
-      <button disabled={props.selectingCardID === "NotSelect" || props.selectingCardID === "Picked"}
+      <Button type="submit"
+              disabled={props.selectingCardID === "NotSelect" || props.selectingCardID === "Picked"}
               onClick={() => {
                 const pickCard = props.me.handCardList.find(h => h.cardID === props.selectingCardID);
                 props.onClickPick({card: pickCard, playerID: props.me.playerID})
               }}>
-        ピックする
-      </button>
+          ピック
+        </Button>
     </span>
   );
 
-  const popover = (
-    <Popover id="popover-trigger-click-root-close" title="デッキレシピ">
+  const pickListPopover = (
+    <Popover id="popover-trigger-click-root-close" title="ピック順に表示">
       {props.me.draftDeckList.map((d, i) => (
         <div key={`deck-${i}`}>{d.name}</div>
       ))}
     </Popover>
   );
 
-  const showDeckButton = (
+  const showPickListButton = (
     <span style={{paddingLeft: 20}}>
       <OverlayTrigger
         trigger="click"
         rootClose
         placement="bottom"
-        overlay={popover}>
-        <Button>デッキレシピ</Button>
+        overlay={pickListPopover}>
+        <Button>ピックリスト</Button>
       </OverlayTrigger>
     </span>
   );
 
-  const playerElm = (
-    <div>
+  const deckListPopover = (
+    <Popover id="popover-trigger-click-root-close" title="カード種別に表示">
+      {props.me.draftDeckList.map((d, i) => (
+        <div key={`deck-${i}`}>{d.name}</div>
+      ))}
+    </Popover>
+  );
+
+  const showDeckListButton = (
+    <span style={{paddingLeft: 20}}>
+      <OverlayTrigger
+        trigger="click"
+        rootClose
+        placement="bottom"
+        overlay={deckListPopover}>
+        <Button>デッキリスト</Button>
+      </OverlayTrigger>
+    </span>
+  );
+
+  const playerListElm = (
+    <div style={{paddingLeft: 20}}>
       {props.players.map((p, i) => {
-        const style = (p.playerID === props.me.playerID) ? {color: "red"} : {color: "black"};
+        const style = (p.playerID === props.me.playerID) ? {color: "red", fontSize: 20} : {color: "black", fontSize: 20};
         return (
           (props.gameProgress === GAME_PROGRESS.SECOND_ROUND) ?
-            <span style={style} key={i}>{`${p.playerName} ← `}</span> : <span style={style} key={i}>{`${p.playerName} → `}</span>
+            <span style={style} key={i}>{`プレイヤー順: ${p.playerName} ← `}</span> : <span style={style} key={i}>{`プレイヤー順: ${p.playerName} → `}</span>
         )
       })}
     </div>
   );
 
+  const cardListElm = (props.me.handCardList.map((c, i) => (
+    <span key={`card-${i}`}>
+              {i % (ROTATION_MAX_NUMBER / 3) === 0 ? <p/> : null}
+      <CardComponent card={c}
+                     onClickCard={cardID => props.onClickCard(cardID)}
+                     selectingCardID={props.selectingCardID}
+                     selectedCardID={props.selectedCardID}/>
+            </span>
+  )));
+
   return (
     <div>
       <br/>
       {pickButton}
-      {showDeckButton}
-      {props.me.handCardList.map((c, i) => (
-          <span key={`card-${i}`}>
-              {i % 7 === 0 ? <p/> : null}
-            <CardComponent card={c}
-                           onClickCard={cardID => props.onClickCard(cardID)}
-                           selectingCardID={props.selectingCardID}
-                           selectedCardID={props.selectedCardID}/>
-            </span>
-        )
-      )}
-      {playerElm}
+      {showPickListButton}
+      {showDeckListButton}
+      <br/>
+      <br/>
+      {playerListElm}
+      {cardListElm}
     </div>
   )
 };

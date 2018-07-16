6d27cf3a6a62e57c131b09fe64a24f2aa23e71aa
genshun <genshun9@gmail.com>
2018-07-16 15:45:00 +0900

    start.shを修正

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@
     find ./dist -name '*.map' -exec bash -c 'mv $0 ./dist/entry.8850c411.map' {} + &&
     rm -rf js &&
     ./node_modules/.bin/tsc &&
+    node js/server/importAssetsCardList.js &&
     echo "セットアップ完了" &&
     node js/server/index.js

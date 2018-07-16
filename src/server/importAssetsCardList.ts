8c9af403ea759f6caefce0d1ba7c92a826666e76
genshun <genshun9@gmail.com>
2018-07-16 14:34:34 +0900

    魔法カードの対応

--- src/server/importAssetsCardList.ts
+++ src/server/importAssetsCardList.ts
@@ -26,3 +26,30 @@ fs.readFile('assets/inputCardList/monster.txt', {encoding: 'utf-8'}, (err1, text
     });
   }
 });
+
+fs.readFile('assets/inputCardList/magic.txt', {encoding: 'utf-8'}, (err1, text) => {
+  if (err1) {
+    throw err1
+  } else {
+    const outputText:{ name: string, cardURL: string, cardID: string, cardType: number}[] = [];
+    const textDatas = text.split('\n').map(d => d.split(','));
+    textDatas.forEach(textData => {
+      for (let i =0; i < textData[2]; i++) {
+        let card = {
+          name: textData[0],
+          cardURL: textData[1],
+          cardID: `${textData[1]}-${Number(i)+1}`,
+          cardType: 2
+        };
+        outputText.push(card);
+      }
+    });
+    fs.writeFileSync('assets/outputCardList/magic.json', JSON.stringify(outputText), (err2) => {
+      if (err2) {
+        throw err2
+      } else {
+        console.log("出力完了");
+      }
+    });
+  }
+});

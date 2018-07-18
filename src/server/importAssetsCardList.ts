0c133536dc29906420ad050c97ca7c9e905bfdc3
genshun <genshun9@gmail.com>
2018-07-19 00:56:40 +0900

    importAssetsCardList内に汎用メソッドを追加

--- src/server/importAssetsCardList.ts
+++ src/server/importAssetsCardList.ts
@@ -1,136 +1,37 @@
-const fs = require('fs');
+import * as fs from 'fs';
 
-fs.readFile('assets/inputCardList/monster.txt', {encoding: 'utf-8'}, (err1, text) => {
-  if (err1) {
-    throw err1
-  } else {
-    const outputText:{ name: string, cardURL: string, cardID: string, cardType: number}[] = [];
-    const textDatas = text.split('\n').map(d => d.split(','));
-    textDatas.forEach(textData => {
-      for (let i =0; i < textData[2]; i++) {
-        let card = {
-          name: textData[0],
-          cardURL: textData[1],
-          cardID: `${textData[1]}-${Number(i)+1}`,
-          cardType: 1
-        };
-        outputText.push(card);
-      }
-    });
-    fs.writeFileSync('assets/outputCardList/monster.json', JSON.stringify(outputText), (err2) => {
-      if (err2) {
-        throw err2
-      } else {
-        console.log("出力完了");
-      }
-    });
-  }
-});
+// index.tsとは別のアプリケーションなので、以下汎用メソッドはutils配下におかずにここの中に閉じ込める。
+/**
+ * inputCardListのtextファイルから、outputCardListにjsonを出力
+ * @param {string} target
+ * @param {number} cardType
+ * @param {boolean} isRare
+ */
+const writeJsonFromTextFile = (target, cardType, isRare) => {
+  fs.readFile(`assets/inputCardList/${target}.txt`, {encoding: 'utf-8'}, (err1, text) => {
+    if (err1) {
+      throw err1
+    } else {
+      const outputText:{ name: string, cardURL: string, cardID: string, cardType: number}[] = [];
+      const textDatas = text.split('\n').map(d => d.split(','));
+      textDatas.forEach(textData => {
+        for (let i =0; i < Number(textData[2]); i++) {
+          let card = {
+            name: textData[0],
+            cardURL: textData[1],
+            cardID: `${textData[1]}-${Number(i)+1}`,
+            cardType: isRare ? Number(textData[3]) : cardType
+          };
+          outputText.push(card);
+        }
+      });
+      fs.writeFileSync(`assets/outputCardList/${target}.json`, JSON.stringify(outputText));
+    }
+  });
+};
 
-fs.readFile('assets/inputCardList/magic.txt', {encoding: 'utf-8'}, (err1, text) => {
-  if (err1) {
-    throw err1
-  } else {
-    const outputText:{ name: string, cardURL: string, cardID: string, cardType: number}[] = [];
-    const textDatas = text.split('\n').map(d => d.split(','));
-    textDatas.forEach(textData => {
-      for (let i =0; i < textData[2]; i++) {
-        let card = {
-          name: textData[0],
-          cardURL: textData[1],
-          cardID: `${textData[1]}-${Number(i)+1}`,
-          cardType: 2
-        };
-        outputText.push(card);
-      }
-    });
-    fs.writeFileSync('assets/outputCardList/magic.json', JSON.stringify(outputText), (err2) => {
-      if (err2) {
-        throw err2
-      } else {
-        console.log("出力完了");
-      }
-    });
-  }
-});
-
-fs.readFile('assets/inputCardList/trap.txt', {encoding: 'utf-8'}, (err1, text) => {
-  if (err1) {
-    throw err1
-  } else {
-    const outputText:{ name: string, cardURL: string, cardID: string, cardType: number}[] = [];
-    const textDatas = text.split('\n').map(d => d.split(','));
-    textDatas.forEach(textData => {
-      for (let i =0; i < textData[2]; i++) {
-        let card = {
-          name: textData[0],
-          cardURL: textData[1],
-          cardID: `${textData[1]}-${Number(i)+1}`,
-          cardType: 3
-        };
-        outputText.push(card);
-      }
-    });
-    fs.writeFileSync('assets/outputCardList/trap.json', JSON.stringify(outputText), (err2) => {
-      if (err2) {
-        throw err2
-      } else {
-        console.log("出力完了");
-      }
-    });
-  }
-});
-
-fs.readFile('assets/inputCardList/extra.txt', {encoding: 'utf-8'}, (err1, text) => {
-  if (err1) {
-    throw err1
-  } else {
-    const outputText:{ name: string, cardURL: string, cardID: string, cardType: number}[] = [];
-    const textDatas = text.split('\n').map(d => d.split(','));
-    textDatas.forEach(textData => {
-      for (let i =0; i < textData[2]; i++) {
-        let card = {
-          name: textData[0],
-          cardURL: textData[1],
-          cardID: `${textData[1]}-${Number(i)+1}`,
-          cardType: 4
-        };
-        outputText.push(card);
-      }
-    });
-    fs.writeFileSync('assets/outputCardList/extra.json', JSON.stringify(outputText), (err2) => {
-      if (err2) {
-        throw err2
-      } else {
-        console.log("出力完了");
-      }
-    });
-  }
-});
-
-fs.readFile('assets/inputCardList/rare.txt', {encoding: 'utf-8'}, (err1, text) => {
-  if (err1) {
-    throw err1
-  } else {
-    const outputText:{ name: string, cardURL: string, cardID: string, cardType: number}[] = [];
-    const textDatas = text.split('\n').map(d => d.split(','));
-    textDatas.forEach(textData => {
-      for (let i =0; i < textData[2]; i++) {
-        let card = {
-          name: textData[0],
-          cardURL: textData[1],
-          cardID: `${textData[1]}-${Number(i)+1}`,
-          cardType: Number(textData[3])
-        };
-        outputText.push(card);
-      }
-    });
-    fs.writeFileSync('assets/outputCardList/rare.json', JSON.stringify(outputText), (err2) => {
-      if (err2) {
-        throw err2
-      } else {
-        console.log("出力完了");
-      }
-    });
-  }
-});
+writeJsonFromTextFile('monster', 1, false);
+writeJsonFromTextFile('magic', 2, false);
+writeJsonFromTextFile('trap', 3, false);
+writeJsonFromTextFile('extra', 4, false);
+writeJsonFromTextFile('rare', 0, true);
